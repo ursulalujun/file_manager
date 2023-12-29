869524faa80e561436b334e28a602de2edf59ac1
huangju <931481086@qq.com>
2023-12-29 20:43:33 +0800

    实现自定义备份

---
+++ FileCopy.java
@@ -0,0 +1,99 @@
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class FileCopy {	
+    public void copy(String srcPath,String dstPath) throws IOException {
+        try {
+            File file=new File(srcPath);
+            if(file.isDirectory()){
+                FileCopyUtils.copyDir(srcPath,dstPath);
+            }
+            else {            	
+                File f=new File(dstPath);
+                if(!f.exists()){
+                    f.createNewFile();
+                }
+                FileCopyUtils.copyFile(srcPath,dstPath);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void diycopy(String srcPath,String dstPath) throws IOException{
+    	try {
+    		File file = new File(srcPath);
+    		if(file.isDirectory()) {
+    			FileDiyCopy.copyDiyDir(srcPath,dstPath);
+    		}
+    		else {
+    			File f = new File(dstPath);
+    			if(!f.exists()) {
+    				f.createNewFile();
+    			}
+    			FileDiyCopy.copyFile(srcPath,dstPath);
+    		}
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+
+    public void compress(String srcPath,String dstPath){
+        try {
+            FileZipUtil.compressFile(srcPath,dstPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unzip(String srcPath, String dstPath) throws Exception {
+        try {
+            UnZipUtil.unzip(srcPath,dstPath);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void encrypy(String srcPath,String dstPath,int key) throws IOException {
+            try {
+                File file=new File(srcPath);
+                if(file.isDirectory()){
+                    FileEncrypyUtils.search(srcPath,dstPath, key);
+                }
+                else {
+                    File f=new File(dstPath);
+                    if(!f.exists()){
+                        f.createNewFile();
+                    }
+                    FileEncrypyUtils.encrypy(srcPath,dstPath,key);
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+    }
+
+    public void decrypy(String srcPath,String dstPath,int key) throws IOException {
+        FileEncrypyUtils.decrypy(srcPath,dstPath,key);
+    }
+
+    public void varify(String firstpath,String secondpath) throws IOException {
+        varifyUtils first=new varifyUtils();
+        varifyUtils second=new varifyUtils();
+        first.search(firstpath,firstpath);
+        second.search(secondpath,secondpath);
+
+//        System.out.println(first.map);
+//        System.out.println(second.map);
+//        if (first.map.equals(second.map)){
+//            System.out.println(true);
+//        }else {
+//            System.out.println(false);
+//        }
+    }
+}
+
